c21367d5076fd1c517c85ad51a70d098de959ce2
Duachee Lor <156361234+DuacheeL@users.noreply.github.com>
2025-02-25 18:45:56 +0000

    Added SVGs in HTML & Gsap Animation -  Added Gsap Animation to JS for one svg

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
 // ✅ gsap loaded? 
 console.log( gsap );
+
+
+// gsap.from("#dataFolder", {
+// 	rotate: 15,
+// 	duration: .5,
+// 	repeat: -1,
+// 	yoyo: true,
+// 	ease: "elastic.out(2, 1)"
+// });
+
+gsap.from("#dataFolder", {
+	x: -180,
+	duration: 1,
+});
+
+// console.log("hello");
+
+gsap.from("#rightBracket",
+	{
+		x: 2.5,
+		duration: .5,
+		repeat: -1,
+		yoyo: true
+	});
+
+
+gsap.from("#leftBracket",
+	{
+		x: -2.5,
+		duration: .5,
+		repeat: -1,
+		yoyo: true
+	});
